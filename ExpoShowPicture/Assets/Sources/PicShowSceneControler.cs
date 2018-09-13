8fd6c740aacd2eca6d13e5d66c08ddd460b72b50
Alexis Trouve <Alexis Trouve>
2018-09-13 22:29:46 +0200

    create showing menu and some data to test things

---
+++ ExpoShowPicture/Assets/Sources/PicShowSceneControler.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct Step
+{
+    public Color32 _imgColor;
+    public Vector2 _anchorMin;
+    public Vector2 _anchorMax;
+
+    public Step(Color32 imgColor, Vector2 anchorMin, Vector2 anchorMax)
+    {
+        _imgColor = imgColor;
+        _anchorMin = anchorMin;
+        _anchorMax = anchorMax;
+    }
+}
+
+public class PicShowSceneControler : MonoBehaviour
+{
+    public InputField infieldRef;
+    public GameObject[] pics;
+    private int _picIndex = 0;
+    private ResourceLoader resources;
+    private Dictionary<string, int> picLink;
+    public int numberStepTimer = 5;
+    private int actualStepTimer = 5;
+    private Step[] steps;
+    private int IndexStep = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        steps = new Step[7];
+        steps[0] = new Step(new Color32(255, 255, 255, 0), new Vector2(0.1f, 0.797f), new Vector2(0.7f, 0.976f));
+        steps[1] = new Step(new Color32(255, 255, 255, 140), new Vector2(0.1f, 0.797f), new Vector2(0.7f, 0.976f));
+        steps[2] = new Step(new Color32(255, 255, 255, 210), new Vector2(0.2f, 0.603f), new Vector2(0.8f, 0.783f));
+        steps[3] = new Step(new Color32(255, 255, 255, 255), new Vector2(0.24f, 0.41f), new Vector2(0.84f, 0.59f));
+        steps[4] = new Step(new Color32(255, 255, 255, 210), new Vector2(0.2f, 0.217f), new Vector2(0.8f, 0.397f));
+        steps[5] = new Step(new Color32(255, 255, 255, 140), new Vector2(0.1f, 0.024f), new Vector2(0.7f, 0.203f));
+        steps[6] = new Step(new Color32(255, 255, 255, 0), new Vector2(0.1f, 0.024f), new Vector2(0.7f, 0.203f));
+        picLink = new Dictionary<string, int>();
+        resources = new ResourceLoader();
+        resources.load();
+        int i = pics.Length - 2;
+        foreach (GameObject go in pics)
+        {
+            --i;
+            if (i >= 0)
+                picLink.Add(go.name, resources._arraydatas[i]._dataref);
+            else
+                picLink.Add(go.name, resources._arraydatas[0]._dataref);
+        }
+        rawUpdatePic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actualStepTimer < numberStepTimer)
+            softUpdatePic();
+        else if (actualStepTimer == numberStepTimer)
+        {
+            ++IndexStep;
+            if (IndexStep >= steps.Length)
+                IndexStep = 0;
+            rawUpdatePic();
+            ++actualStepTimer;
+            ++_picIndex;
+        }
+    }
+
+    void softUpdatePic()
+    {
+        int stepIndex = IndexStep;
+        int picIndex = 0;
+        int nextStepIndex = stepIndex + 1;
+        if (nextStepIndex == steps.Length)
+            nextStepIndex = 0;
+
+        do
+        {
+            RectTransform rect = pics[picIndex].GetComponent<RectTransform>();
+            rect.anchorMin = new Vector2(rect.anchorMin.x + (steps[nextStepIndex]._anchorMin.x - steps[stepIndex]._anchorMin.x) / numberStepTimer
+                , rect.anchorMin.y + (steps[nextStepIndex]._anchorMin.y - steps[stepIndex]._anchorMin.y) / numberStepTimer);
+            rect.anchorMax = new Vector2(rect.anchorMax.x + (steps[nextStepIndex]._anchorMax.x - steps[stepIndex]._anchorMax.x) / numberStepTimer
+                , rect.anchorMax.y + (steps[nextStepIndex]._anchorMax.y - steps[stepIndex]._anchorMax.y) / numberStepTimer);
+            rect.offsetMin = new Vector2(0, 0);
+            rect.offsetMax = new Vector2(0, 0);
+            Image img = pics[picIndex].GetComponent<Image>();
+            img.color = new Color32((byte)((byte)(img.color.r * 255) + (byte)((steps[nextStepIndex]._imgColor.r - steps[stepIndex]._imgColor.r) / numberStepTimer))
+                , (byte)((byte)(img.color.g * 255) + (byte)((steps[nextStepIndex]._imgColor.g - steps[stepIndex]._imgColor.g) / numberStepTimer))
+                , (byte)((byte)(img.color.b * 255) + (byte)((steps[nextStepIndex]._imgColor.b - steps[stepIndex]._imgColor.b) / numberStepTimer))
+                , (byte)((byte)(img.color.a * 255) + (byte)((steps[nextStepIndex]._imgColor.a - steps[stepIndex]._imgColor.a) / numberStepTimer)));
+            if (stepIndex != 0 && stepIndex != steps.Length - 1)
+                pics[picIndex].transform.SetAsLastSibling();
+            ++stepIndex;
+            ++picIndex;
+            if (stepIndex == steps.Length)
+                stepIndex = 0;
+            nextStepIndex = stepIndex + 1;
+            if (nextStepIndex == steps.Length)
+                nextStepIndex = 0;
+        } while (stepIndex != IndexStep);
+        ++actualStepTimer;
+    }
+
+    void rawUpdatePic()
+    {
+        int stepIndex = IndexStep;
+        int picIndex = 0;
+
+        do
+        {
+            RectTransform rect = pics[picIndex].GetComponent<RectTransform>();
+            rect.anchorMin = steps[stepIndex]._anchorMin;
+            rect.anchorMax = steps[stepIndex]._anchorMax;
+            rect.offsetMin = new Vector2(0, 0);
+            rect.offsetMax = new Vector2(0, 0);
+            Image img = pics[picIndex].GetComponent<Image>();
+            img.color = steps[stepIndex]._imgColor;
+            if (stepIndex == 0)
+                picLink[pics[picIndex].name] = resources._arraydatas[(_picIndex + 4) % resources._datas.Count]._dataref;
+            img.sprite = resources._datas[picLink[pics[picIndex].name]].getSpriteIndex(0);
+            if (stepIndex != 0 && stepIndex != steps.Length - 1)
+                pics[picIndex].transform.SetAsLastSibling();
+            ++stepIndex;
+            ++picIndex;
+            if (stepIndex == pics.Length)
+                stepIndex = 0;
+        } while (stepIndex != IndexStep);
+    }
+
+    public void nextStep()
+    {
+        rawUpdatePic();
+        actualStepTimer = 0;
+    }
+
+    public void clickNewPic(int index)
+    {
+        Debug.Log("pic " + index + " : " + pics[index].name + " = " + picLink[pics[index].name]);
+    }
+}
---
+++ ExpoShowPicture/Assets/Sources/ResourceLoader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct picData
+{
+    public int _dataref;
+    public Dictionary<int, Sprite> _datasub;
+
+    public picData(int dataref, int datasubref, string spriteFileName)
+    {
+        _dataref = dataref;
+        _datasub = new Dictionary<int, Sprite>();
+        Texture2D texture = Resources.Load("TestImgs/" + spriteFileName) as Texture2D;
+        texture.Apply();
+        _datasub.Add(datasubref, Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1));
+    }
+
+    public void addataSub(int subref, string spriteFileName)
+    {
+        Texture2D texture = Resources.Load("TestImgs/" + spriteFileName) as Texture2D;
+        texture.Apply();
+        _datasub.Add(subref, Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1));
+    }
+
+    public Sprite getSpriteIndex(int index)
+    {
+        foreach (KeyValuePair<int, Sprite> elem in _datasub)
+        {
+            if (index <= 0)
+                return (elem.Value);
+            --index;
+        }
+        return (null);
+    }
+}
+
+public class ResourceLoader
+{
+    public Dictionary<int, picData> _datas;
+    public picData[] _arraydatas;
+
+    public void load()
+    {
+        _datas = new Dictionary<int, picData>();
+        _datas.Add(0, new picData(0, 0, "arid"));
+        _datas.Add(1, new picData(1, 1, "hot desert"));
+        _datas.Add(2, new picData(2, 0, "coast"));
+        _datas.Add(3, new picData(3, 0, "cold desert"));
+        _datas.Add(4, new picData(4, 0, "ContinentaleGrass"));
+        _datas[4].addataSub(1, "NordicGrass");
+        _datas[4].addataSub(2, "OceanicGrass");
+        _datas[4].addataSub(3, "mediteraneanGrass");
+        _datas.Add(5, new picData(5, 0, "jungle"));
+        _datas.Add(6, new picData(6, 0, "mountain"));
+        _datas.Add(7, new picData(7, 0, "snow mountain"));
+        _datas.Add(8, new picData(8, 0, "NordicCoast"));
+        _datas.Add(9, new picData(9, 0, "riverwater coast"));
+        _datas.Add(10, new picData(10, 0, "sea"));
+        _arraydatas = new picData[_datas.Count];
+        int i = 0;
+        foreach (KeyValuePair<int, picData> data in _datas)
+        {
+            _arraydatas[i] = data.Value;
+            ++i;
+        }
+    }
+}
